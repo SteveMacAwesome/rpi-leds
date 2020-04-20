aeda6ff78e3fb8f29404a2a1c38dde95eec9c4e1
Ben Allen <benjaminallen88@gmail.com>
2020-04-20 16:32:11 +0200

    Tidy up express a bit, add POST route

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,63 @@ import { bounceFunction } from './ledstrip/bounce';
 import { RgbColour } from './ledstrip/util';
 import { solidFunction } from './ledstrip/solid';
 
-// const express = require('express')
 import * as express from 'express';
+
 const webServer = express();
-const port = 3000
 
 const args = process.argv.slice(2);
 const colour: RgbColour = {
-  red: parseInt(args[0] || '0'),
+  red: parseInt(args[0] || '255'),
   green: parseInt(args[1] || '0'),
-  blue: parseInt(args[2] || '0'),
+  blue: parseInt(args[2] || '119'),
 }
-const ledCount = parseInt(args[3] || '300');
-const animInterval = parseInt(args[4] || '150');
+const ledCount = parseInt(args[3] || '120');
+const animInterval = parseInt(args[4] || '50');
 
 const ledStrip = initStrip({
   leds: ledCount,
   strip: 'grb'
 });
 
-const bounce = bounceFunction(ledCount, colour, 20);
-const solid = solidFunction(ledCount, colour);
+const bounce = bounceFunction(ledCount, colour, 10);
+const solid = solidFunction(ledCount, {red: 0, green: 0, blue: 0});
 
-const interval = setInterval(() => {
-  ledStrip.render(bounce());
-}, animInterval); // tadaa
+let interval;
 
+const startBounce = () => {
+  return setInterval(() => {
+    ledStrip.render(bounce());
+  }, animInterval); // tadaa
+}
 
-webServer.get('/', (req, res) => {
-  console.log('got request on port 3000, clearing interval and rendering solid');
-  clearInterval(interval);
+const port = 3000;
 
-  ledStrip.render(solid());
-  res.send('Hello World!')
-});
+webServer
+  .use(express.json())
+  .get('/', (req, res) => {
+    console.log('Received GET request, restarting animation\n');
+    if (interval) {
+      clearInterval(interval);
+    }
 
-webServer.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+    interval = startBounce();
+    res.send('Restarted bounce animation\n').status(200).end();
+  })
+  .post('/', function (req, res) {
+    console.log('Received POST request:', req.body);
+
+    if (interval) {
+      clearInterval(interval);
+    }
+
+    const {
+      red = 128,
+      green = 120,
+      blue = 100
+    } = req.body;
+
+    ledStrip.render(solid({red, green, blue}));
+
+    res.send(`Displaying #${red.toString(16)}${green.toString(16)}${blue.toString(16)}\n`).status(200).end();
+  })
+  .listen(port, () => console.log(`Webserver listening on http://localhost:${port}`));
--- src/ledstrip/solid.ts
+++ src/ledstrip/solid.ts
@@ -1,7 +1,7 @@
-import { RgbColour, rgbToInt } from './util'
+import { rgbToInt, RgbColour } from './util'
 
-export const solidFunction = (ledCount: number, colour: RgbColour) => () => {
+export const solidFunction = (ledCount: number, colour: RgbColour) => (overrideColour?: RgbColour) => {
   const pixels = new Uint32Array(ledCount);
-  const colourInt = rgbToInt(colour);
+  const colourInt = rgbToInt(overrideColour ? overrideColour : colour);
   return pixels.map(() => colourInt);
 }
